369931f91d09a4aa317635e1a41a059f4e335e57
Xandra Mentink <xandra1400@hotmail.com>
2017-09-14 19:40:55 +0200

    commit 4

---
+++ Exercise 2 les 2/Opdracht 2.4.py
@@ -0,0 +1,5 @@
+uurloon =  eval(input('Wat verdien je per uur: '))
+aantaluur = eval(input('Hoeveel uur heb je gewerkt: '))
+print(str(aantaluur), 'uren werken levert' + ' ' +  str(uurloon), 'euro op.' )
+
+line3 = str(aantaluur), ' uren werken levert ' + str(uurloon), 'euro op.'
